fcb179125b915a905d2ff00e3c19a4a03d959d99
whitestranger7 <vezhnovec2000@mail.ru>
2018-12-12 21:01:20 +0300

    v1.0

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 <b>My first test game named "Pig Game"</b><br>
 
+------------------------------------------------------------
+
+Rules:<br>
+
+Active player make turn rolling dice (it rolled randomly). That dice get summed in current score of active player.
+If active player rolled 1 - his current score getting lost and become 0. And active player will be switched. 
+So active player can hold his current score and set it to global score and save his score. After holding score the active player will be switched and his current score will become 0 again, but global score remain. Players will roll dices as long as one of them haven't 100 in global score.
+When "New Game" button was clicked - the game will be set up to new game<br>
+
+------------------------------------------------------------
+
 v0.1:<br>
 <ul>
     <li>Main structure of game is done for now and ready to use.</li>
-    <li>Next update will include game logic using JS. It will be ready for playing soon</li>
+    <li>Next update will include game logic using JavaScript. It will be ready for playing soon</li>
+</ul><br>
+
+v1.0 (Stable App):<br>
+<ul>
+    <li>The app is available for playing the Pig Game. script.js was added to game folder (content/js/script.js)</li>
+    <li>Info button still unable to use. It'll become as soon as posible</li>
 </ul>
--- content/js/script.js
+++ content/js/script.js
@@ -1,1 +1,95 @@
-//Coming soon...
+
+var scores, current, activePlayer, gameplaying;
+
+//function which is exicuting with starting new game
+var init = function () {
+
+    scores = [0,0];
+    current = 0;
+    activePlayer = Math.floor(Math.random() * 2);
+    gameplaying = true;
+    document.querySelector('.player-0-panel').classList.remove("active");
+    document.querySelector('.player-1-panel').classList.remove("active");
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
+    document.querySelector('.dice').style.display = "none";
+    document.querySelector('#current-0').textContent = 0;
+    document.querySelector('#current-1').textContent = 0;
+    document.querySelector('#score-0').textContent = 0;
+    document.querySelector('#score-1').textContent = 0;
+    document.querySelector('#name-0').textContent = 'Player 1';
+    document.querySelector('#name-1').textContent = 'Player 2';
+
+}
+
+//function which is change the active player
+var changePlayer = function() {
+
+    if (activePlayer == 0){
+        activePlayer = 1;
+    } else if (activePlayer == 1){
+        activePlayer = 0;
+    }
+    document.querySelector('.player-0-panel').classList.toggle("active");
+    document.querySelector('.player-1-panel').classList.toggle("active");
+    document.querySelector('#current-0').textContent = 0;
+    document.querySelector('#current-1').textContent = 0;
+}
+
+//function which is exicuting when roll button was clicked
+var roll = function () {
+
+    if (gameplaying){
+        //1. Roll random number
+        var dice;
+        dice = Math.floor(Math.random() * 6) + 1;   
+
+        //2. Display the result and set current score
+        document.querySelector('.dice').src = 'content/img/dice-' + dice + '.png';
+        document.querySelector('.dice').style.display = "inherit";
+        current += dice;
+
+        //3.Update current score and sum it if dice is not 1
+        if (dice == 1) {
+            current = 0;
+            document.querySelector('#current-' + activePlayer).textContent = current;
+            changePlayer();
+
+        } else {
+            document.querySelector('#current-' + activePlayer).textContent = current;
+        }
+    }
+
+}
+
+//function which is invoked when hold button was clicked
+var hold = function() {
+
+    if (gameplaying){
+        //Add current score to global score
+        scores[activePlayer] += current;
+
+        //Update UI
+        document.querySelector('.dice').style.display = "none";
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        //Check if player win the game
+        if(scores[activePlayer] >= 100){
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gameplaying = false;
+        } else {
+            //Set current score to 0 and switch active player
+            current = 0;
+            changePlayer();
+        }
+    }
+
+}
+
+
+//functions exicution
+init();
+document.querySelector('.btn-roll').addEventListener('click', roll);
+document.querySelector('.btn-new').addEventListener('click', init);
+document.querySelector('.btn-hold').addEventListener('click', hold);
